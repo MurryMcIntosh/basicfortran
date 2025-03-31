ad42709d431e85dd125ce5cb3e8676daf52d0240
Brandon Murry <murry.brandon@fcboe.org>
2025-03-31 15:47:38 -0400

    added datatypes

---
+++ datatypes/datatypes.f90
@@ -0,0 +1,26 @@
+!  datatypes.f90 
+!
+!  FUNCTIONS:
+!  datatypes - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: datatypes
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program datatypes
+
+    implicit none
+    integer :: age, weight
+    ! Variables
+
+    
+    ! Body of datatypes
+    print *, 'Hello World'
+
+    end program datatypes
+
